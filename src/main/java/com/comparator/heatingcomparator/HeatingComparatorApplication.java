1a039ed42ae0e22582cc995d69b641f1fad5d9ae
vincentferraro <v.ferraro@live.fr>
2024-01-29 14:21:30 -0500

    second commit

---
+++ src/main/java/com/comparator/heatingcomparator/HeatingComparatorApplication.java
@@ -0,0 +1,13 @@
+package com.comparator.heatingcomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatingComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatingComparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comparator/heatingcomparator/HeatingComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comparator.heatingcomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatingComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
